f09e6d1eec2350f48b010dd3d3558543686911e9
Lashirova <60476768+Lashirova@users.noreply.github.com>
2020-10-06 21:20:51 -0400

    calculation of dog years

---
+++ script.js
@@ -0,0 +1,26 @@
+// Year is given//
+let myAge = 20;
+let myAge = prompt('What is your age?');
+let earlyYears = 2;
+// calculate dog year//
+earlyYears = earlyYears * 10,5;
+laterYears = myAge -2;
+earlyYears = earlyYears * 10,5;
+//laterYears result has been set
+let lateryears = myAge -2;
+// Each year following equates to 4dog's years
+laterYears * = 4;
+console.log(earlyYears); 
+console.log(laterYears);
+// Ages are stored
+let  myAgeInDogYears = earlyYears + laterYears;
+let myName = 'JHON'
+console.log(myName.toLowerCase()); 
+// print name in lowercase
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.
+  `);
+  // display my name abd age in dog years.
+  
+  alert(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.
+  `);
+  
